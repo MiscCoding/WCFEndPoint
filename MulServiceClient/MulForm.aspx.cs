e9aa275edab1b18ac8f671802af1364f82ff9cec
neogeoss <neogeo-s@hanmail.net>
2019-01-21 00:12:05 +0900

    초기 커밋.

---
+++ MulServiceClient/MulForm.aspx.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using MulServiceReference;
+
+public partial class MulForm : System.Web.UI.Page
+{
+    protected void Page_Load(object sender, EventArgs e)
+    {
+
+    }
+
+    protected void Button1_Click(object sender, EventArgs e)
+    {
+        MulServiceClient MSC = new MulServiceClient("BasicHttpBinding_IMulService");
+        Response.Write(MSC.Mul(5, 5));
+    }
+}
